a1265659dcd761a32ed00fbf4048d5bbcb613879
edgarrobles <edgarrobles991@gmail.com>
2014-10-03 17:18:55 -0700

    finsh video 8

---
+++ public_html/js/snake.js
@@ -0,0 +1,23 @@
+var snake;
+
+var context;
+var screenwith;
+var screenheight;
+
+function gameinitialize() {
+   var canvas = document .getelementbyid("game-sreen"); 
+   context = canvas.getcontext("2d");
+   
+   screenwith = window.innerWidth;
+   screenheight = window.innerHeight;
+   
+   canvas.width = screenwidth;
+}
+
+function gameloop() {
+    
+}
+
+function gamedrow (){
+    
+}
