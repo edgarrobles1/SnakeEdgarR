ee6ea105176862489372fe64bd58f321c020f774
edgarrobles <edgarrobles991@gmail.com>
2014-10-06 12:12:35 -0700

    fixing

--- public_html/js/snake.js
+++ public_html/js/snake.js
@@ -4,6 +4,9 @@ var context;
 var screenwith;
 var screenheight;
 
+
+
+
 function gameinitialize() {
    var canvas = document .getelementbyid("game-sreen"); 
    context = canvas.getcontext("2d");
@@ -12,6 +15,7 @@ function gameinitialize() {
    screenheight = window.innerHeight;
    
    canvas.width = screenwidth;
+   canvas.height= screenheight;
 }
 
 function gameloop() {
@@ -19,5 +23,6 @@ function gameloop() {
 }
 
 function gamedrow (){
-    
+    context.fillstyle="rgb(180,250,213)";
+    context.fillrect(0,0 , screenheight, screenheight);
 }
